4a415ece88c7fa8278c88c29261b74f8dece8a97
szj2ys <15574364821@163.com>
2021-07-31 12:10:53 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+## 第一次先执行下面命令安装一些包
+# npm install hexo-deployer-git --save
+# npm install hexo-generator-search --save
+
+hexo clean
+hexo generate
+
+hexo server # 本地预览
+hexo deploy  # 部署到Git上
+
+
+# hexo new "文章题目"  # 创建新文章
+# hexo new page "页面名称"  # 创建新页面
+
+
+
+
+
+
+# # 快捷命令
+# hexo g == hexo generate
+# hexo d == hexo deploy
+# hexo s == hexo server
+# hexo n == hexo new
+
+
---
+++ scaffolds/post.md
@@ -0,0 +1,11 @@
+---
+title: {{ title }}
+date: {{ date }}
+tags:
+categories:
+keywords:
+description:
+top_img:
+cover:
+comments: false
+---
